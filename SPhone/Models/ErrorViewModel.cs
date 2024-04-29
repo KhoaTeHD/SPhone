7bc1c46f14f410512474af7161af404b40397fb3
TeHD <dangkhoa014@gmail.com>
2024-04-29 20:33:30 +0700

    Add project files.

---
+++ SPhone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SPhone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
